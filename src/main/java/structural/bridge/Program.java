53c76ff43079613f445c960384df68d079d61a4f
bobr <bobrus91@gmai.com>
2023-10-17 00:43:56 +0300

    comments were reformatted from rus to eng

--- src/main/java/structural/bridge/Program.java
+++ src/main/java/structural/bridge/Program.java
@@ -4,8 +4,8 @@ import structural.bridge.colors.BlackColor;
 import structural.bridge.shapes.Triangle;
 
 public class Program {
-    //Класс Shape - абстракция
-    //Интерфейс Colorable - реализация
+    //Class Shape - abstraction
+    //Interface Colorable - realization
     public static void main(String[] args) {
         Triangle triangle = new Triangle(new BlackColor());
         triangle.draw();
